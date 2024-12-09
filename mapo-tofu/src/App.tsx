60c8939b012afb9f8c8ce60a16fe8943f19b65fc
Todd Jacobus <todd@Todds-MacBook-Air.local>
2024-12-08 17:20:09 -0800

    adds mantine, appshell, some snazy ui elements

--- mapo-tofu/src/App.tsx
+++ mapo-tofu/src/App.tsx
@@ -1,21 +1,49 @@
-
+import { AppShell, Burger, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import './App.css';
+
 import Map from 'react-map-gl/maplibre';
 
+import { useDisclosure } from '@mantine/hooks';
+
 function App() {
+  const [opened, { toggle }] = useDisclosure(false);
 
   return (
-    <>
-      <Map
-        initialViewState={{
-          longitude: -121.06,
-          latitude: 47.32,
-          zoom: 6
+    <MantineProvider>
+      <AppShell
+        header={{ height: 60}}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened},
         }}
-        style={{width: 1200, height: 800}}
-        mapStyle="https://api.maptiler.com/maps/topo-v2/style.json?key=1fpqJCS5obaeWPdWYoZ6"
-      />
-    </>
+        padding='md'
+      >
+        <AppShell.Header>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            // hiddenFrom='sm'
+            size='sm'
+          />
+        </AppShell.Header>
+        <AppShell.Navbar>Navbar</AppShell.Navbar>
+        <AppShell.Main>
+          <Map
+            initialViewState={{
+              longitude: -121.06,
+              latitude: 47.32,
+              zoom: 6
+            }}
+            style={{width: 400, height: 400}}
+            mapStyle="https://api.maptiler.com/maps/outdoor-v2/style.json?key=1fpqJCS5obaeWPdWYoZ6"
+          />
+        </AppShell.Main>
+      </AppShell>
+
+      
+    </MantineProvider>
   )
 }
 
